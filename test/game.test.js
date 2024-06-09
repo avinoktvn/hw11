57de4a074006bffd0ffc4496b1834ab616cb2055
avinoktvn <avinjvl@gmail.com>
2024-06-09 18:31:25 +0700

    unit testing

--- test/game.test.js
+++ test/game.test.js
@@ -4,9 +4,9 @@ const { sequelize } = require("../models");
 const { queryInterface } = sequelize;
 
 // Dijalankan sebelum automatic API testing
-beforeAll((done) => {
-  queryInterface
-    .bulkInsert("Games", [
+beforeAll(async () => {
+  try {
+    await queryInterface.bulkInsert("Games", [
       {
         id: 1001,
         title: "AAA",
@@ -34,85 +34,47 @@ beforeAll((done) => {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-    ])
-    .then(() => {
-      done();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+    ]);
+  } catch (err) {
+    console.log(err);
+  }
+}, 30000); // Set timeout to 30000ms (30 seconds)
 
 // Dijalankan setelah automatic API testing
-afterAll((done) => {
-  // Cleaning Database
-
-  queryInterface
-    .bulkDelete("Games", null, {})
-    .then(() => {
-      done();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+afterAll(async () => {
+  try {
+    await queryInterface.bulkDelete("Games", null, {});
+  } catch (err) {
+    console.log(err);
+  }
+}, 30000); // Set timeout to 30000ms (30 seconds)
 
 // Automatic API TESTING
-
 describe("GET Games", () => {
-  it("List Games", (done) => {
-    // supertest
-    request(app)
-      .get("/games")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((response) => {
-        expect(response.body.length).toEqual(3);
-        const games = response.body;
-        const firstGame = games[0];
-        expect(firstGame.id).toEqual(1001);
-        expect(firstGame.title).toEqual("AAA");
-        done();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  it("List Games", async () => {
+    const response = await request(app).get("/games").expect("Content-Type", /json/).expect(200);
+
+    expect(response.body.length).toEqual(3);
+    const games = response.body;
+    const firstGame = games[0];
+    expect(firstGame.id).toEqual(1001);
+    expect(firstGame.title).toEqual("AAA");
   });
 
-  it("Get Detail Games", (done) => {
-    // supertest
-    request(app)
-      .get(`/games/${1001}`)
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((response) => {
-        console.log(response.body, "<<<<<<");
-        const game = response.body;
-        expect(game.id).toEqual(1001);
-        expect(game.title).toEqual("AAA");
-        expect(game.year).toEqual(2022);
-        expect(game.developer).toEqual("AAA");
-        done();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  it("Get Detail Games", async () => {
+    const response = await request(app).get(`/games/${1001}`).expect("Content-Type", /json/).expect(200);
+
+    const game = response.body;
+    expect(game.id).toEqual(1001);
+    expect(game.title).toEqual("AAA");
+    expect(game.year).toEqual(2022);
+    expect(game.developer).toEqual("AAA");
   });
 
-  it("Test Error Not Found", (done) => {
-    // supertest
-    request(app)
-      .get(`/games/${1001123123}`)
-      .expect("Content-Type", /json/)
-      .expect(404)
-      .then((response) => {
-        const { message } = response.body;
+  it("Test Error Not Found", async () => {
+    const response = await request(app).get(`/games/${1001123123}`).expect("Content-Type", /json/).expect(404);
 
-        expect(message).toBe("Error Not Found");
-        done();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const { message } = response.body;
+    expect(message).toBe("Error Not Found");
   });
 });
