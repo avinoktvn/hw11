91dddf12f38e3dec6d190916b99fc65284341bea
avinoktvn <avinjvl@gmail.com>
2024-06-20 14:58:34 +0700

    model

--- models/game.js
+++ models/game.js
@@ -1,45 +1,27 @@
-// "use strict";
-// const { Model } = require("sequelize");
-// module.exports = (sequelize, DataTypes) => {
-//   class Game extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
-//   }
-//   Game.init(
-//     {
-//       title: DataTypes.STRING,
-//       developer: DataTypes.STRING,
-//       year: DataTypes.INTEGER,
-//       genres: DataTypes.STRING,
-//     },
-//     {
-//       sequelize,
-//       modelName: "Game",
-//     }
-//   );
-//   return Game;
-// };
-
 "use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  const Games = sequelize.define(
-    "Games",
+  class Game extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Game.init(
     {
       title: DataTypes.STRING,
       developer: DataTypes.STRING,
-      genres: DataTypes.STRING,
       year: DataTypes.INTEGER,
+      genres: DataTypes.STRING,
     },
-    {}
+    {
+      sequelize,
+      modelName: "Game",
+    }
   );
-  Games.associate = function (models) {
-    // associations can be defined here
-  };
-  return Games;
+  return Game;
 };
