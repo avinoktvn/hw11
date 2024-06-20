d043ed0ba0cfd1208c252c770d4b826fb77cd396
avinoktvn <avinjvl@gmail.com>
2024-06-20 13:38:40 +0700

    unit testing

--- test/game.test.js
+++ test/game.test.js
@@ -85,9 +85,9 @@ const { sequelize } = require("../models");
 const { queryInterface } = sequelize;
 
 // Dijalankan sebelum automatic API testing
-beforeAll(async () => {
-  try {
-    await queryInterface.bulkInsert("Games", [
+beforeAll((done) => {
+  queryInterface
+    .bulkInsert("Games", [
       {
         id: 1001,
         title: "AAA",
@@ -115,47 +115,127 @@ beforeAll(async () => {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-    ]);
-  } catch (err) {
-    console.log(err);
-  }
-}, 30000); // Set timeout to 30000ms (30 seconds)
+    ])
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // Dijalankan setelah automatic API testing
-afterAll(async () => {
-  try {
-    await queryInterface.bulkDelete("Games", null, {});
-  } catch (err) {
-    console.log(err);
-  }
-}, 30000); // Set timeout to 30000ms (30 seconds)
+afterAll((done) => {
+  // Cleaning Database
+
+  queryInterface
+    .bulkDelete("Games", null, {})
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // Automatic API TESTING
+
 describe("GET Games", () => {
-  it("List Games", async () => {
-    const response = await request(app).get("/games").expect("Content-Type", /json/).expect(200);
-
-    expect(response.body.length).toEqual(3);
-    const games = response.body;
-    const firstGame = games[0];
-    expect(firstGame.id).toEqual(1001);
-    expect(firstGame.title).toEqual("AAA");
+  it("List Games", (done) => {
+    // supertest
+    request(app)
+      .get("/games")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.length).toEqual(3);
+        const games = response.body;
+        const firstGame = games[0];
+        expect(firstGame.id).toEqual(1001);
+        expect(firstGame.title).toEqual("AAA");
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
-  it("Get Detail Games", async () => {
-    const response = await request(app).get(`/games/${1001}`).expect("Content-Type", /json/).expect(200);
-
-    const game = response.body;
-    expect(game.id).toEqual(1001);
-    expect(game.title).toEqual("AAA");
-    expect(game.year).toEqual(2022);
-    expect(game.developer).toEqual("AAA");
+  it("Get Detail Games", (done) => {
+    // supertest
+    request(app)
+      .get(`/games/${1001}`)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        console.log(response.body, "<<<<<<");
+        const game = response.body;
+        expect(game.id).toEqual(1001);
+        expect(game.title).toEqual("AAA");
+        expect(game.year).toEqual(2022);
+        expect(game.developer).toEqual("AAA");
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
-  it("Test Error Not Found", async () => {
-    const response = await request(app).get(`/games/${1001123123}`).expect("Content-Type", /json/).expect(404);
+  it("Test Error Not Found", (done) => {
+    // supertest
+    request(app)
+      .get(`/games/${1001123123}`)
+      .expect("Content-Type", /json/)
+      .expect(404)
+      .then((response) => {
+        const { message } = response.body;
 
-    const { message } = response.body;
-    expect(message).toBe("Error Not Found"); // Corrected to match the controller
+        expect(message).toBe("Error Not Found");
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+});
+
+describe("CREATE GAME", () => {
+  it("Create Game", (done) => {
+    request(app)
+      .post(`/games`)
+      .send({
+        title: "DDD",
+        developer: "DDD",
+        year: 2020,
+        genres: "Action RPG",
+      })
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .then((response) => {
+        const { message } = response.body;
+
+        expect(message).toBe("Game created successfully");
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+});
+
+describe("DELETE GAME", () => {
+  it("Delete Game", (done) => {
+    request(app)
+      .delete(`/games/${1003}`)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        const { message } = response.body;
+
+        expect(message).toBe("Game deleted successfully");
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 });
